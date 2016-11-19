0eb95b420398ea02a44c81ba6287d2efd0df1729
Shay McDowell <smcdowel@andrew.cmu.edu>
2016-11-20 00:55:28 +1030

    Shay Update Saturday Night
    Updated Scene 6 (Created Scene)

--- QCAS/src/qcas/QCAS.java
+++ QCAS/src/qcas/QCAS.java
@@ -19,7 +19,7 @@ public class QCAS extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Scene3.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("scene6.fxml"));
         
         Scene scene = new Scene(root);
         
---
+++ QCAS/src/qcas/Scene6Controller.java
@@ -0,0 +1,24 @@
+
+package qcas;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shay McDowell // Scene 6 // Teacher Report Scene
+ */
+public class Scene6Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
