37dae6248d7a3a2846e695e37e1d231af60e5af7
Shay McDowell <smcdowel@andrew.cmu.edu>
2016-11-21 20:42:47 +1030

    Merge origin/master
    Conflicts:
    	QCAS/src/qcas/LoginScreenController.java
    	QCAS/src/qcas/Scene2Controller.java
    	QCAS/src/qcas/scene2.fxml

--- QCAS/src/qcas/TeacherUploadController.java
+++ QCAS/src/qcas/TeacherUploadController.java
@@ -1,11 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package qcas;
-
-
 import au.com.bytecode.opencsv.CSVReader;
 import java.io.BufferedReader;
 import java.io.File;
@@ -59,10 +52,12 @@ public class TeacherUploadController implements Initializable {
     private Label textLabel;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }
 
     
